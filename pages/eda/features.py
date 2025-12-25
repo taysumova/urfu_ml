d08df424eaf1ce25a6df0936461294df342ea3ed
Luiza <luitays@mail.ru>
2025-12-25 13:53:49 +0300

    final collab

--- pages/eda/features.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-import matplotlib.pyplot as plt
-
-st.header("Анализ категориальных признаков")
--- pages/model/model.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-st.header("Model")
-st.text("Coming soon")
